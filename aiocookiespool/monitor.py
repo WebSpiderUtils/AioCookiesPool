793e6f4a3165a5094dd5409b7bf7806c745375e9
huaiyukeji <huaiyukeji@gmail.com>
2023-12-15 10:44:46 +0800

    refactor: monitor

--- aiocookiespool/monitor.py
+++ aiocookiespool/monitor.py
@@ -15,26 +15,26 @@ class Monitor:
         self.blocklist_db = RedisClient('blocklist', self.website)
         self.tasks = []
 
-    async def task(self, username: str, sem: Semaphore) -> NoReturn:
+    async def task(self, username: str, item: dict, sem: Semaphore) -> NoReturn:
         async with sem:
-            if await self.blocklist_db.delete(username):
-                logger.info(f'剔除黑名单：{username}')
+            logger.info(f'正在检查账号：{username}')
+            enter_time = item.get('timestamp')
+            total_time = item.get('total_time')
+            now = int(time.time())
+            slept_time = now - enter_time
+            remaining_time = total_time - slept_time
+            if slept_time >= total_time:
+                if await self.blocklist_db.delete(username):
+                    logger.info(f'剔除黑名单：{username}')
+            else:
+                logger.info(f'仍需睡眠：{username} 剩余时间：{remaining_time} 秒')
 
     async def run(self):
         sem = Semaphore(MONITOR_CONCURRENCE)
         blocklist_groups = await self.blocklist_db.all()
         for username, item in blocklist_groups.items():
-            logger.info(f'正在检查账号：{username}')
-            item2json = json.loads(item)
-            enter_time = item2json.get('timestamp')
-            total_time = item2json.get('total_time')
-            now = int(time.time())
-            slept_time = now - enter_time
-            remaining_time = total_time - slept_time
-            if slept_time >= total_time:
-                self.tasks.append(self.task(username, sem))
-            else:
-                logger.info(f'仍需睡眠：{username} 剩余时间：{remaining_time} 秒')
+            item = eval(item)
+            self.tasks.append(self.task(username, item, sem))
         await gather(*self.tasks)
         logger.success("黑名单检查结束")
 
